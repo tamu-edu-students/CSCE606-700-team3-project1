716b66714e9216283637ad5ad88d6e55d3332676
adamharrison-tamu <adamharrison@tamu.edu>
2026-09-13 13:54:03 -0500

    Added more tests to #add in main_spec.rb

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,45 +1,61 @@
 require_relative '../lib/profresh'
+require 'json'
+require 'date'
 
 describe 'adding new task' do
 
     describe '#add' do
-
-        # Story 1 ------------------------------------------------------------
 
         it 'should be defined' do
             title = "write code"
             priority = "high"
             date_due = "9/26/2026"
             tags = [""]
-            expect {add(title, priority, date_due, tags)}.not_to raise_error
+            expect {add(title, priority, date_due)}.not_to raise_error
         end
 
-        # it 'creates json file if it doesn't exist yet'
-            # expect file to exist
+        let(:file_path) {File.join(__dir__, '..', 'data', 'task_list.json')}
+        let(:parsed_json) do
+            file_content = File.read(file_path)
+            JSON.parse(file_content)
+        end
 
-        # it 'is stored correctly into json file'
-            # expect title = title
-            # expect priority = priority
-            # etc... to include timestamps, status, and id
+        it 'json file exists and has tasks array' do
+            expect(File.exist?(file_path)).to be true
+            expect {parsed_json}.not_to raise_error
+            expect(parsed_json).to have_key('tasks')
+            expect(parsed_json['tasks']).to be_an(Array)
+            expect(parsed_json['tasks']).not_to be_empty
+        end
 
-        # --------------------------------------------------------------------
+        it 'stores correct data type in json file' do
+            parsed_json['tasks'].each do |task|
+                expect(task).to include('id' => be_a(Integer))
+                expect(task).to include('title' => be_a(String))
+                expect(task).to include('priority' => be_a(String))
+                expect(task).to include('date_due' => be_a(String))
+                expect(task).to include('tags' => be_an(Array))
+                expect(task).to include('status' => be_a(String))
+                expect(task).to include('created' => be_a(String))
+                expect(task).to include('updated' => be_a(String))
+                expect(task).to include('finished' => be_a(String))
+            end
+        end
 
-        # Story 3 ------------------------------------------------------------
-
-        # it 'properly handles date input'
-            # expect obviously incorrect date to raise error
-
-        # it 'stores date in proper format'
-            # expect date stored in the proper format: ISO 8601'
-
-        # --------------------------------------------------------------------
+        it 'date stored in proper format ISO8601' do
+            parsed_json['tasks'].each do |task|
+                expect {Date.iso8601(task['date_due'])}.not_to raise_error
+                expect {Date.iso8601(task['created'])}.not_to raise_error
+                expect {Date.iso8601(task['updated'])}.not_to raise_error
+                unless task['finished'].empty?
+                    expect {Date.iso8601(task['finished'])}.not_to raise_error
+                end
+            end
+        end
 
     end
 
-
     # describe '#add_tag'
-
-        # Story 2 ------------------------------------------------------------
 
         # it 'should be defined'
             # expect not to raise error
@@ -48,19 +64,12 @@ describe 'adding new task' do
             # expect to be true
             # expect to eq 'CSCE 606'
 
-        # --------------------------------------------------------------------
-
-
     # describe '#edit'
-
-        # Story 4 ------------------------------------------------------------
 
         # it 'should be defined'
             # expect not to raise error
 
         # it 'properly saves changes'
             # expect priority to eq 'high'
-
-        # --------------------------------------------------------------------
 
 end
