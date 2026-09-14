95632e4c2eb93dc345db81f16cb640c09061075f
adamharrison-tamu <adamharrison@tamu.edu>
2026-09-14 12:14:56 -0500

    Tests completed for stories 1 thru 4

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,32 +2,44 @@ require_relative '../lib/profresh'
 require 'json'
 require 'date'
 
-describe 'Adding new task' do
+describe 'Add and edit tasks and tags' do
 
-    describe '#add' do
+    # Retrieve json file and contents to be used in testing
+    def task_file_path
+        File.join(__dir__, '..', 'data', 'task_list.json')
+    end
 
-        # Unit Tests for Stories 1 & 3
+    def delete_task_by_id(target_id)
+        path = task_file_path
+        return unless target_id && File.exist?(path)
 
-
-        id = nil # so we can keep what the expect-add returns and be sure to delete it at end of testing
-
-
-        # Test add method is defined
-        it 'should be defined' do
-            title = "write code"
-            priority = "high"
-            date_due = "9/26/2026"
-            expect {
-                id = add(title, priority, date_due)
-            }.not_to raise_error
+        data = JSON.parse(File.read(path))
+        if data['tasks']
+            data['tasks'].reject! {|task| task['id'] == target_id}
+            File.write(path, JSON.pretty_generate(data))
         end
+    end
 
+    describe '#add_task' do
 
-        # Retrieve json file and contents to be used in testing
-        let(:file_path) {File.join(__dir__, '..', 'data', 'task_list.json')}
+        # Unit Tests for Stories 1, 2, 3, & 4
+
+        let(:file_path) { task_file_path }
         let(:parsed_json) do
             file_content = File.read(file_path)
             JSON.parse(file_content)
+        end
+
+        before(:all) do
+            title = "write code"
+            priority = "high"
+            date_due = "9/26/2026"
+            @id = add_task(title, priority, date_due)
+        end
+
+        # Test add method is defined
+        it 'should be defined' do
+            expect(@id).not_to be_nil
         end
 
 
@@ -70,79 +82,69 @@ describe 'Adding new task' do
 
         # Cleanup - Remove task created for testing from json file
         after(:all) do
-            if id && File.exist?(file_path)
-                udpated_tasks = parsed_json.reject {|task| task['id'] == id }
-                File.write(file_path, JSON.pretty_generate(updated_tasks))
-            end
+            delete_task_by_id(@id)
         end
 
     end
 
-end
-
-
-describe 'Adding tag to task' do
-
-    title = "write code"
-    priority = "high"
-    date_due = "9/26/2026"
-    id = add(title, priority, date_due)
-
-    let(:file_path) {File.join(__dir__, '..', 'data', 'task_list.json')}
-    let(:parsed_json) do
-        file_content = File.read(file_path)
-        JSON.parse(file_content)
-    end
 
     describe '#add_tag' do
 
-        it 'adds tag to task' do
-            expect {add_tag(id, "CSCE 606")}.not_to raise_error
+        let(:file_path) { task_file_path }
+        let(:parsed_json) do
+            file_content = File.read(file_path)
+            JSON.parse(file_content)
+        end
 
-            task = file_content.find {|task| task['id'] == id}
+        before(:all) do
+            title = "write code b"
+            priority = "high"
+            date_due = "9/26/2026"
+            @id = add_task(title, priority, date_due)
+        end
+
+        it 'adds tag to task' do
+            expect {add_tag(@id, "CSCE 606")}.not_to raise_error
+
+            task = parsed_json['tasks'].find {|task| task['id'] == @id}
             expect(task).not_to be_nil
             expect(task['tags']).to include('CSCE 606')
         end
-    end
 
-    after(:all) do
-        if id && File.exist?(file_path)
-            udpated_tasks = parsed_json.reject {|task| task['id'] == id }
-            File.write(file_path, JSON.pretty_generate(updated_tasks))
+        after(:all) do
+            delete_task_by_id(@id)
         end
+
     end
 
-end
-
-
-
-describe 'editing tasks' do
-
-    title = "write stories"
-    priority = "medium"
-    date_due = "9/26/2026"
-    id = add(title, priority, date_due)
-
-    let(:file_path) {File.join(__dir__, '..', 'data', 'task_list.json')}
-    let(:parsed_json) do
-        file_content = File.read(file_path)
-        JSON.parse(file_content)
-    end
 
     describe '#edit_priority' do
+
+
+        let(:file_path) {File.join(__dir__, '..', 'data', 'task_list.json')}
+        let(:parsed_json) do
+            file_content = File.read(file_path)
+            JSON.parse(file_content)
+        end
+
+        before(:all) do
+            title = "write stories"
+            priority = "medium"
+            date_due = "9/26/2026"
+            @id = add_task(title, priority, date_due)
+        end
+
         it 'changes the priority of the "write stories" task to high' do
-            expect {edit_priority(id, "high") }.not_to raise_error
-            task = file_content.find {|task| tasks['id'] == id}
+            expect {edit_priority(@id, "high") }.not_to raise_error
+            task = parsed_json['tasks'].find {|task| task['id'] == @id}
             expect(task).not_to be_nil
             expect(task['priority']).to eq('high')
         end
-    end
 
-    after(:all) do
-        if id && File.exist?(file_path)
-            udpated_tasks = parsed_json.reject {|task| task['id'] == id }
-            File.write(file_path, JSON.pretty_generate(updated_tasks))
+        after(:all) do
+            delete_task_by_id(@id)
         end
+
     end
 
 end
